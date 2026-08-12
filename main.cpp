e592b7b5c462afad8c6a661d3f09a7393450819e
Ougi <emyquaranta@gmail.com>
2026-08-12 18:57:11 +0200

    Disable testing single direction

--- main.cpp
+++ main.cpp
@@ -696,13 +696,11 @@ int main()
     std::vector<std::pair<double, int>> scoredDirections;
 
     // Temporarily test only direction 0 instead of all 100 directions.
-    const std::vector<int> directionIndicesToTest = {0};
+    // const std::vector<int> directionIndicesToTest = {0};
 
-    // for (const auto& direction : fibNormals) {
-    for (const int directionIndex : directionIndicesToTest) {
+    // for (const int directionIndex : directionIndicesToTest) {
+    for (uint directionIndex = 0; directionIndex < fibNormals.size(); ++directionIndex) {
         const auto& direction = fibNormals[directionIndex];
-        // const int directionIndex =
-        //     static_cast<int>(&direction - &fibNormals[0]);
 
         std::cout << "Processing direction: "
                   << direction << "\n";
